ae4d11caf440d7314dff3ca236c991dd14e40ee0
Ochirbat Tsanjid <tsanjidbatochirbat@gmail.com>
2025-12-25 20:00:12 +0800

    project ehenii hubisagch uusgesen

---
+++ app.js
@@ -0,0 +1,42 @@
+//  toglogchiin eeljiig  hadgalah huvisagch, negdvgeer toglogch 0,hoerdugaar toglogchiig 1 gej tendegley,
+
+var activPlayer = 1;
+
+
+
+
+
+
+// toglogchiin tsugluulsan onoog hadgalah huvsagch 
+var scors = [0, 0,];
+
+
+// toglochiin eeljendee tsugluulj baigaa onooog hadgalah huwisagch 
+var roundScore = 0;
+
+
+
+
+// shoo ali talaaraa buusaniig hadgalah huvisagch heregtei , 1-6 gesen utagiig ene huvisagchid sanamsarguigeer vvsgej ogno 
+
+
+var dice = Math.floor(Math.random() * 6) + 1;
+
+
+//  <div class="player-score" id="score-0">43</div>
+
+// window.document.querySelector('#score-0').textContent = dice;
+
+// window.document.querySelector('#score-1').textContent = dice;
+
+
+window.document.querySelector('#score-0').textContent = '0';
+window.document.querySelector('#score-1').textContent = '0';
+
+window.document.querySelector('#current-0').textContent = '0';
+window.document.querySelector('#current-1').textContent = '0';
+
+
+document.querySelector('.dic').style.dicplay = 'none';
+
+console.log(' shoo :' + dice);
